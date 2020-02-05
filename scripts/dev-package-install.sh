b3cc4744a22506ec3140fc7c14693ac87935fcee
Andrea Ricchi <aricchi95@gmail.com>
2020-02-05 20:27:43 +0100

    Move Tilix installation to package script
    Signed-off-by: Andrea Ricchi <aricchi95@gmail.com>

--- scripts/dev-package-install.sh
+++ scripts/dev-package-install.sh
@@ -3,7 +3,15 @@
 # Packages
 sudo apt install -y build-essential git gcc gcc-multilib g++ g++-multilib gdb \
 	vim repo clang-format git-extras picocom libncurses5-dev libgl1-mesa-dev \
-	parcellite cppcheck kdiff3
+	parcellite cppcheck kdiff3 tilix
+
+# Tilix config
+sudo update-alternatives --config x-terminal-emulator
+
+mkdir -p ~/.config/tilix/schemes
+wget https://github.com/clrxbl/tilix-snazzy/raw/master/snazzy.json -O ~/.config/tilix/schemes/snazzy.json
+
+sudo ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
 
 # Git config
 git config --global credential.helper store
--- scripts/zsh-customize.sh
+++ scripts/zsh-customize.sh
@@ -10,13 +10,3 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd -P)"
 ZSHRC_TEMPLATE=${SCRIPT_DIR}/../template/zshrc-template
 cp ${ZSHRC_TEMPLATE} ~/.zshrc
-
-# Tilix
-sudo apt install tilix  || (echo "Tilix installation failed" && exit 1)
-
-sudo update-alternatives --config x-terminal-emulator  || (echo "Tilix default terminal setup failed" && exit 1)
-
-mkdir -p ~/.config/tilix/schemes
-wget https://github.com/clrxbl/tilix-snazzy/raw/master/snazzy.json -O ~/.config/tilix/schemes/snazzy.json  || (echo "Tilix snazzy-theme installation failed" && exit 1)
-
-sudo ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh  || (echo "Tilix links creation failed" && exit 1)
