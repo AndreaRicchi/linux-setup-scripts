5e072d39edfa4f450c3801ceed0174ae5d6ddbfa
Andrea Ricchi <aricchi95@gmail.com>
2020-02-05 17:20:06 +0100

    Add KDiff3 package and add git config
    Signed-off-by: Andrea Ricchi <aricchi95@gmail.com>

--- scripts/dev-package-install.sh
+++ scripts/dev-package-install.sh
@@ -3,5 +3,8 @@
 # Packages
 sudo apt install -y build-essential git gcc gcc-multilib g++ g++-multilib gdb \
 	vim repo clang-format git-extras picocom libncurses5-dev libgl1-mesa-dev \
-	parcellite cppcheck 
+	parcellite cppcheck kdiff3
 
+# Git config
+git config --global credential.helper store
+git config --global --add merge.tool kdiff3
