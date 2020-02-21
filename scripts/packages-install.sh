5f890b63ea45488d31b1b5ee9d312c61a1f5bd23
Andrea Ricchi <aricchi95@gmail.com>
2020-02-21 15:10:34 +0100

    Remove Brave installation
    Signed-off-by: Andrea Ricchi <aricchi95@gmail.com>

--- scripts/packages-install.sh
+++ scripts/packages-install.sh
@@ -18,15 +18,11 @@ wget https://github.com/clrxbl/tilix-snazzy/raw/master/snazzy.json -O ~/.config/
 sudo ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
 
 # Brave browser
-sudo apt install apt-transport-https curl
-
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-
-sudo apt update
-
-sudo apt install brave-browser
+#sudo apt install apt-transport-https curl
+#curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+#echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+#sudo apt update
+#sudo apt install brave-browser
 
 # Git config
 git config --global credential.helper store
