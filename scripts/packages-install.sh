3967916e094f54fe67d23c76abdbee968b29ef60
Andrea Ricchi <aricchi95@gmail.com>
2020-02-27 11:39:35 +0100

    Update package install script
    Signed-off-by: Andrea Ricchi <aricchi95@gmail.com>

--- scripts/packages-install.sh
+++ scripts/packages-install.sh
@@ -7,22 +7,11 @@ sudo apt full-upgrade
 # Packages
 sudo apt install -y build-essential git gcc gcc-multilib g++ g++-multilib gdb \
 	vim repo clang-format git-extras picocom libncurses5-dev libgl1-mesa-dev \
-	parcellite cppcheck kdiff3 tilix
+	cppcheck tilix kdiff3-qt
 
 # Tilix config
 sudo update-alternatives --config x-terminal-emulator
-
-mkdir -p ~/.config/tilix/schemes
-wget https://github.com/clrxbl/tilix-snazzy/raw/master/snazzy.json -O ~/.config/tilix/schemes/snazzy.json
-
 sudo ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
-
-# Brave browser
-#sudo apt install apt-transport-https curl
-#curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-#echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-#sudo apt update
-#sudo apt install brave-browser
 
 # Git config
 git config --global credential.helper store
