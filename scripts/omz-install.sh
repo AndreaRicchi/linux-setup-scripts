75c05dbe028376763219537e36949e27c63c21af
Andrea Ricchi <aricchi95@gmail.com>
2020-03-09 16:44:42 +0100

    Add reboot message after OMZ installation
    Signed-off-by: Andrea Ricchi <aricchi95@gmail.com>

--- scripts/omz-install.sh
+++ scripts/omz-install.sh
@@ -3,3 +3,4 @@
 # Oh My Zsh
 sudo apt install -y zsh curl || (echo "Zsh/curl installation failed" && exit 1)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || (echo "Oh My Zsh installation failed" && exit 1)
+echo "System reboot is required"
