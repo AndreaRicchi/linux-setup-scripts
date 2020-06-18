41b09fe6586a17dd77ac60902e61c9cde64518eb
Andrea Ricchi <aricchi95@gmail.com>
2020-06-18 15:46:36 +0200

    Update OhMyZsh installation script
    Signed-off-by: Andrea Ricchi <aricchi95@gmail.com>

--- scripts/omz-install.sh
+++ scripts/omz-install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-# Oh My Zsh
-sudo apt install -y zsh curl || (echo "Zsh/curl installation failed" && exit 1)
+# oh-my-zsh deps
+sudo apt install -y zsh git curl git-extras || (echo "Zsh deps installation failed" && exit 1)
+
+# oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || (echo "Oh My Zsh installation failed" && exit 1)
-echo "System reboot is required"
+
