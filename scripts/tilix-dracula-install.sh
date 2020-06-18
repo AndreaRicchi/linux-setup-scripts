be3dd700da27139301071924f16171a9b437230a
Andrea Ricchi <aricchi95@gmail.com>
2020-06-18 15:45:36 +0200

    Add dedicated Tilix installation script
    Signed-off-by: Andrea Ricchi <aricchi95@gmail.com>

---
+++ scripts/tilix-dracula-install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# install
+sudo apt install tilix || (echo "Tilix installation failed" && exit 1)
+
+# tilix config
+sudo update-alternatives --config x-terminal-emulator || (echo "Tilix configuration failed" && exit 1)
+sudo ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh || (echo "Symbolic link creation failed" && exit 1)
+
+# tilix dracula theme
+mkdir -p ~/Projects/dracula
+git clone https://github.com/dracula/tilix.git ~/Projects/dracula/tilix || (echo "Tilix dracula theme clone failed" && exit 1)
+mkdir -p ~/.config/tilix/schemes
+ln -s ~/Projects/dracula/tilix/Dracula.json ~/.config/tilix/schemes/Dracula.json || (echo "Symbolic link creation failed" && exit 1)
