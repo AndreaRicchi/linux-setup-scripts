04d7946ba2cc375b7867dcd744ce75250f87c6c5
Andrea Ricchi <aricchi95@gmail.com>
2020-07-04 20:23:53 +0200

    Fix QtCreator dracula theme link creation
    Signed-off-by: Andrea Ricchi <aricchi95@gmail.com>

--- scripts/qt-dracula-install.sh
+++ scripts/qt-dracula-install.sh
@@ -4,4 +4,4 @@
 mkdir -p ~/Projects/dracula
 git clone https://github.com/dracula/qtcreator.git ~/Projects/dracula/qtcreator || (echo "QtCreator dracula theme clone failed" && exit 1)
 mkdir -p ~/.config/QtProject/qtcreator/styles
-ln -s ~/Projects/dracula/qtcreator/dracula.json ~/.config/QtProject/qtcreator/styles/dracula.xml || (echo "Symbolic link creation failed" && exit 1)
+ln -s ~/Projects/dracula/qtcreator/dracula.xml ~/.config/QtProject/qtcreator/styles/dracula.xml || (echo "Symbolic link creation failed" && exit 1)
