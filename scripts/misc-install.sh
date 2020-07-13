3b49cc2c6533dcfb44a0e1b3ff43cc437ac1ae4f
Andrea Ricchi <aricchi95@gmail.com>
2020-07-13 20:51:07 +0200

    Add openvpn network manager install
    Signed-off-by: Andrea Ricchi <aricchi95@gmail.com>

--- scripts/misc-install.sh
+++ scripts/misc-install.sh
@@ -7,7 +7,7 @@ sudo apt install vim -y
 sudo apt install gcc gcc-multilib g++ g++-multilib cppcheck clang-format gdb -y
 
 # Tools
-sudo apt install picocom git libgl1-mesa-dev fonts-firacode python pv tree iotop bmap-tools -y
+sudo apt install picocom git libgl1-mesa-dev fonts-firacode python pv tree iotop bmap-tools network-manager-openvpn -y
 
 # General setup
 sudo adduser $(whoami) dialout
