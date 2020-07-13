d96594567009f17c4156e90494e481a42c9041a9
Andrea Ricchi <aricchi95@gmail.com>
2020-07-13 20:45:10 +0200

    Set VIM as default GIT editor
    Signed-off-by: Andrea Ricchi <aricchi95@gmail.com>

--- scripts/misc-install.sh
+++ scripts/misc-install.sh
@@ -9,5 +9,6 @@ sudo apt install gcc gcc-multilib g++ g++-multilib cppcheck clang-format gdb -y
 # Tools
 sudo apt install picocom git libgl1-mesa-dev fonts-firacode python pv tree iotop bmap-tools -y
 
-# Dialout setup
+# General setup
 sudo adduser $(whoami) dialout
+git config --global core.editor "vim"
