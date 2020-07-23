96d57a080322050a9e8e990e3e57b607e2964f31
Andrea Ricchi <aricchi95@gmail.com>
2020-07-23 13:55:15 +0200

    brave-install: remove repository add command ouput
    Signed-off-by: Andrea Ricchi <aricchi95@gmail.com>

--- scripts/brave-install.sh
+++ scripts/brave-install.sh
@@ -11,9 +11,9 @@ echo "done"
 
 printf "Adding repository to apt... "
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc \
-	| sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add - || { echo "fail"; exit 1; }
+	| sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add - >/dev/null 2>&1 || { echo "fail"; exit 1; }
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" \
-	| sudo tee /etc/apt/sources.list.d/brave-browser-release.list || { echo "fail"; exit 1; }
+	| sudo tee /etc/apt/sources.list.d/brave-browser-release.list >/dev/null 2>&1 || { echo "fail"; exit 1; }
 echo "done"
 
 printf "Installing Brave... "
