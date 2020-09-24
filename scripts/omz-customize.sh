cdf5838578b6c5596663d4966a4b8c8ede9c3408
Andrea Ricchi <aricchi95@gmail.com>
2020-09-24 10:08:00 +0200

    omz-customize: fix indentation
    Signed-off-by: Andrea Ricchi <aricchi95@gmail.com>

--- scripts/omz-customize.sh
+++ scripts/omz-customize.sh
@@ -16,29 +16,29 @@ install_dracula_theme()
 
 install_starship_prompt()
 {
-        # zsh starship prompt
+	# zsh starship prompt
 	curl -fsSL https://starship.rs/install.sh | bash
 
-        # zshrc file
-        ZSHRC_TEMPLATE=${SCRIPT_DIR}/../template/zshrc-staship-template
-        cp "${ZSHRC_TEMPLATE}" ~/.zshrc
+	# zshrc file
+	ZSHRC_TEMPLATE=${SCRIPT_DIR}/../template/zshrc-staship-template
+	cp "${ZSHRC_TEMPLATE}" ~/.zshrc
 }
 
 PS3='Please enter your choice: '
 options=("Dracula Theme" "Starship Prompt")
 select opt in "${options[@]}"
 do
-    case $opt in
-        "Dracula Theme")
-		install_dracula_theme
-		break
-		;;
-        "Starship Prompt")
-		install_starship_prompt
-		break
-		;;
-        *) echo "invalid option $REPLY";;
-    esac
+	case $opt in
+		"Dracula Theme")
+			install_dracula_theme
+			break
+			;;
+		"Starship Prompt")
+			install_starship_prompt
+			break
+			;;
+		*) echo "invalid option $REPLY";;
+	esac
 done
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting \
