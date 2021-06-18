4747b6dfae22905f425c769d311fc6a1a344885f
Andrea Ricchi <aricchi95@gmail.com>
2021-06-18 12:36:58 +0200

    Add $HOME variant for zsh-syntax-highlighting clone
    Signed-off-by: Andrea Ricchi <aricchi95@gmail.com>

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -44,7 +44,7 @@ zsh_theme_plugins_install() {
 	echo
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k
 	echo
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
 }
 
 RESOURCESPATH=$SCRIPTPATH/../resources
